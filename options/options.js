3c29f415327e6437488803ffe0284ea0e34b721d
Crowdbotics <team@crowdbotics.com>
2024-06-10 09:05:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "image-detector-48332",
+  url: "https://image-detector-48332.botics.co",
+  api: "https://image-detector-48332.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
